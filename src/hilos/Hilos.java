f6ecd690ebb1cfbddd0ae5c0946c2474ca88a71c
BrayanPinzon <brayanpin56@gmail.com>
2022-11-11 19:46:10 -0500

    hola

---
+++ src/hilos/Hilos.java
@@ -0,0 +1,28 @@
+package hilos;
+
+public class Hilos extends Thread {
+
+    private String name;
+    private int retardo;
+
+    public Hilos(String s, int d) {
+        name = s;
+        retardo = d;
+
+    }
+    
+    @Override
+    public void run() {
+
+        try {
+
+            sleep(retardo);
+
+        } catch (InterruptedException e) {
+
+        }
+
+        System.out.println("Prueba Hilos: " + name + " " + retardo);
+
+    }
+}
